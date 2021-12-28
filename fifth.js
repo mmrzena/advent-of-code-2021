90955ab405afc6bfa76f68a0a8210a393f6657e6
Michal Mrzena <michal.mrzena@socialbakers.com>
2021-12-28 10:58:51 +0100

    fifth part 2

--- fifth.js
+++ fifth.js
@@ -26,18 +26,32 @@ import { readFile } from './utils.js'
 			for(let i = Math.min(y1, y2); i <= Math.max(y1, y2); i++){
 				list.push({ x: x1, y: i })
 			}
-		}
-
-		if (y1 === y2) {
+		} else if (y1 === y2) {
 			for(let i = Math.min(x1, x2); i <= Math.max(x1, x2); i++){
 				list.push({ x: i, y: y1 })
 			}
+		} else {
+			const xList = []
+			const yList = []
+			// 9,7 -> 7,9 covers points 9,7, 8,8, and 7,9.
+			for(let i = Math.min(x1, x2); i <= Math.max(x1, x2); i++){
+				xList.push(i)
+			}
+			for(let i = Math.min(y1, y2); i <= Math.max(y1, y2); i++){
+				yList.push(i)
+			}
+			
+			xList.forEach((x, index) => {
+				list.push({
+					x: Math.min(x1, x2) === x1 ? x : xList[xList.length -1 - index],
+					y: Math.min(y1, y2) === y1 ? yList[index] : yList[yList.length -1 - index]
+				})
+			})
 		}
 
 		return list
 	}
-	const coordinatesOfEveryPoint = dataPartOne.map(calculatePointsBetween).flat()
-	const mapOfPointsCrossed = coordinatesOfEveryPoint.reduce((acc, current) => {
+	const getMapOfPointsCrossed = (acc, current) => {
 		const key = JSON.stringify(current)
 		if (acc[key]) {
 			acc[key] += 1
@@ -45,10 +59,15 @@ import { readFile } from './utils.js'
 			acc[key] = 1
 		}
 		return acc
-	}, {})
+	}
 
+	const coordinatesOfEveryPoint = dataPartOne.map(calculatePointsBetween).flat()
+
+	const mapOfPointsCrossed = coordinatesOfEveryPoint.reduce(getMapOfPointsCrossed, {})
 	const finalNumberOfPointsOverlapped = Object.values(mapOfPointsCrossed).filter((number) => number > 1).length
 	
 	// part two
-
+	const coordinatesOfEveryPointPart2 = parsedData.map(calculatePointsBetween).flat()
+	const mapOfPointsCrossedPart2 = coordinatesOfEveryPointPart2.reduce(getMapOfPointsCrossed, {})
+	const finalNumberOfPointsOverlappedPart2 = Object.values(mapOfPointsCrossedPart2).filter((number) => number > 1).length
 })()
